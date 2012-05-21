df523b1c9016307270af063123c6892bb661bd56
Angel Koilov <angel.koilov@gmail.com>
2012-05-22 00:16:30 +0300

    override some methods. @TODO onchange trigger

--- src/Ptbfw/Selenium2Driver/Selenium2Driver.php
+++ src/Ptbfw/Selenium2Driver/Selenium2Driver.php
@@ -11,7 +11,7 @@ class Selenium2Driver extends \Behat\Mink\Driver\Selenium2Driver {
 
 	private $retries;
 	private $retryWait;
-	
+
 	public function __construct($options = array()) {
 		$defaultOptions = array(
 			'browserName' => 'firefox',
@@ -23,9 +23,13 @@ class Selenium2Driver extends \Behat\Mink\Driver\Selenium2Driver {
 
 		$options = array_merge($defaultOptions, $options);
 		parent::__construct($options['browserName'], $options['desiredCapabilities'], $options['wdHost']);
-		
+
 		$this->retries = $options['retries'];
 		$this->retryWait = $options['retryWait'];
+	}
+
+	public function getContent() {
+		throw new \Exception('not implemented yet');
 	}
 
 	/**
@@ -36,17 +40,101 @@ class Selenium2Driver extends \Behat\Mink\Driver\Selenium2Driver {
 	 * @return  array           array of Behat\Mink\Element\NodeElement
 	 */
 	public function find($xpath) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function executeJsOnXpath($xpath, $script, $sync = true) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function getTagName($xpath) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function getText($xpath) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function getHtml($xpath) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function getAttribute($xpath, $attr) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function getValue($xpath) {
+		return $this->retry(__METHOD__, func_get_args(), true);
+	}
+
+	public function setValue($xpath, $value) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function check($xpath) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function uncheck($xpath) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function isChecked($xpath) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function selectOption($xpath, $value, $multiple = false) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function click($xpath) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function executeScript($script) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	public function evaluateScript($script) {
+		return $this->retry(__METHOD__, func_get_args());
+	}
+
+	private function retry($callback, $params = null, $requireReturn = false) {
+
+		$callback = preg_replace('/^.*\:(.*)$/', '$1', $callback);
+
 		$return = null;
 		$tries = 0;
+		$ex = null;
 		do {
-			if ($tries == 0) {
+			if ($tries > 0) {
+				echo PHP_EOL . "{$tries}/{$this->retries} retries" . PHP_EOL;
+			}
+			
+			// wait only if there is a fail
+			if ($tries > 0) {
 				$this->wait($this->retryWait);
 			}
 			$tries++;
-			$return = parent::find($xpath);
-		} while(empty($return) && $tries < $this->retries);
+			try {
+				$return = call_user_func_array(array('parent', $callback), $params);
+				$ex = null;
+			} catch (\Exception $e) {
+				$ex = $e;
+			}
+		} while (empty($return) && $tries <= $this->retries && $requireReturn);
+
+		if ($ex !== null) {
+			throw $ex;
+		}
+		
+		if ($return === null && $requireReturn) {
+			throw new \Exception("error, probably not found element");
+		}
+		
+		return $return;
 	}
-	
+
 	public function wait($time, $condition = 'false') {
 		parent::wait($time, $condition);
 	}
